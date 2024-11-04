9a9a9fb5f62e3154b1c54982bd68227b48c533cd
webshuriken <webshuriken@gmail.com>
2024-11-04 13:34:38 +0000

    feat: Implement profanity checker for the Listing model
    Make sure no bad words are added to title or description. Closes #4

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.exceptions import ValidationError
+from better_profanity import profanity
 
 
 # custom validator used by Listing model
@@ -8,16 +9,21 @@ def validate_price(value):
     if value < 0 or value == 0:
         raise ValidationError(f"{value} is not a valid price")
 
+# custom validator used by Listing model
+def validate_profanity(text):
+    if profanity.contains_profanity(text):
+        raise ValidationError(f"Contains a bad word")
+
 class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000)
+    title = models.CharField(max_length=100, validators=[validate_profanity])
+    description = models.CharField(max_length=1000, validators=[validate_profanity])
     value = models.DecimalField(max_digits=12, decimal_places=2, validators=[validate_price])
     image = models.URLField(blank=True, null=True)
-    user = models.ForeignKey(default=None, on_delete=models.CASCADE, related_name="creator")
-    category = models.ForeignKey(default=None, on_delete=models.CASCADE, related_name="category")
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE, related_name="creator")
+    # category = models.ForeignKey(default=None, on_delete=models.CASCADE, related_name="category")
 
     # custom string representation
     def __str__(self):
