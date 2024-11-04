9cc2af2851b74e8a8e1d1ec12821a16ef5dc4afe
webshuriken <webshuriken@gmail.com>
2024-11-04 12:51:43 +0000

    feat: Add custom price validator for Listing model
    Checks to make sure the price is not zero or minues value

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
 
+
+# custom validator used by Listing model
+def validate_price(value):
+    if value < 0 or value == 0:
+        raise ValidationError(f"{value} is not a valid price")
 
 class User(AbstractUser):
     pass
@@ -8,7 +14,7 @@ class User(AbstractUser):
 class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    value = models.DecimalField(max_digits=12, decimal_places=2)
+    value = models.DecimalField(max_digits=12, decimal_places=2, validators=[validate_price])
     image = models.URLField(blank=True, null=True)
     user = models.ForeignKey(default=None, on_delete=models.CASCADE, related_name="creator")
     category = models.ForeignKey(default=None, on_delete=models.CASCADE, related_name="category")
