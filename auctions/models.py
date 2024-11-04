724f00c928b92414e331316c4fcf826e35120c3a
webshuriken <webshuriken@gmail.com>
2024-11-04 12:04:13 +0000

    feat: Implement field level validation for Listing model

--- auctions/models.py
+++ auctions/models.py
@@ -6,12 +6,12 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    title = models.CharField()
-    description = models.CharField()
-    value = models.DecimalField()
-    image = models.URLField()
-    user = models.ForeignKey()
-    category = models.ForeignKey()
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    value = models.DecimalField(max_digits=12, decimal_places=2)
+    image = models.URLField(blank=True, null=True)
+    user = models.ForeignKey(default=None, on_delete=models.CASCADE, related_name="creator")
+    category = models.ForeignKey(default=None, on_delete=models.CASCADE, related_name="category")
 
     # custom string representation
     def __str__(self):
