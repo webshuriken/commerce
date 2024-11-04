6dd22d8e447e26f517ad9beed688b6c9d3e89aed
webshuriken <webshuriken@gmail.com>
2024-11-04 11:16:57 +0000

    feat: Add Listing model
    Introduces a Listing model with fields for title, description, value, image, user, and category. Resolves #2, resolves #3

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField()
+    description = models.CharField()
+    value = models.DecimalField()
+    image = models.URLField()
+    user = models.ForeignKey()
+    category = models.ForeignKey()
+
+    # custom string representation
+    def __str__(self):
+        return f"ID: {self.id}: {self.title}\nDescription: {self.description}\nValue: {self.value}\nCreated by: {self.user}\n"
