b63140d26e6eb15591532d4d212f28c0a1308636
webshuriken <webshuriken@gmail.com>
2024-11-06 09:30:10 +0000

    feat: Register Comment model in admin.py
    Closes #13

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Listing                                 
+from .models import User, Listing, Comment                            
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Listing)
+admin.site.register(Comment)
