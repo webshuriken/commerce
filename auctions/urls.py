df59885502a0f53ca2211c8dc375f015efe9dd0e
webshuriken <webshuriken@gmail.com>
2024-11-12 23:07:01 +0000

    feat: Add another path to index but with category_id parameter
    This will allow us to use the same template to display a list of all active listings and also a list of all active listings in the same category. ^_^.

--- auctions/urls.py
+++ auctions/urls.py
@@ -7,6 +7,7 @@ app_name = "auctions"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:category_id>", views.index, name="index_with_category"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
