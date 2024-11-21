7b6f19ae0efa485ef5cbc5b96fbdb66d139ccbaa
webshuriken <webshuriken@gmail.com>
2024-11-21 11:27:19 +0000

    fix: title max_length of form to match max_length of model

--- auctions/forms.py
+++ auctions/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Category
 
 class NewListingForm(forms.Form):
-    title = forms.CharField(label="Title", max_length=256, widget=forms.TextInput(attrs={'placeholder': 'Enter listing title'}))
+    title = forms.CharField(label="Title", max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter listing title'}))
     description = forms.CharField(label="Description", max_length=1024, widget=forms.Textarea(attrs={'placeholder': 'Describe your listing'}))
     starting_bid = forms.DecimalField(label="Starting Bid", max_digits=8, decimal_places=2, widget=forms.NumberInput(attrs={'placeholder': 'Enter starting bid'}))
     image_url = forms.URLField(label="Image URL", max_length=512, required=False, widget=forms.URLInput(attrs={'placeholder': 'Enter image URL'}))
